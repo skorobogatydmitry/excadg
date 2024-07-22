58c58de45f88e2e7342293e311b4a7054bbe17da
skorobogatydmitry <skorobogaty.dmitry@gmail.com>
2024-07-22 22:26:25 +0300

    add a new class to track execution graph with basic functionality

---
+++ lib/excadg/vtracker.rb
@@ -0,0 +1,54 @@
+# frozen_string_literal: true
+
+require 'rgl/adjacency'
+
+module ExcADG
+  # tracker for {Vertex}-es graph:
+  # it's hooked by {Broker} to register dependencies polling events
+  # and make an actual graph of vertices with states in runtime
+  #
+  # it's not possible to do this in other way, because vertices can be spawned:
+  # - in any order => code is not guaranteed to know the full graph (even static one)
+  # - dynamically => there is no central place but {Broker} that's aware of all vertices
+  class VTracker
+    attr_reader :graph, :by_state
+
+    def initialize
+      @graph = RGL::DirectedAdjacencyGraph.new
+      @by_state = {}
+    end
+
+    # register the vertex and its new known deps in the @graph and by_state cache
+    # @param vertice vertice that requested info about deps
+    # @param deps list of dependencies as supplied by {Request::GetStateData}
+    def track vertex, deps
+      Assertions.is_a? vertex, Vertex
+      Assertions.is_a? deps, Array
+
+      add_to_states_cache vertex, vertex.state
+
+      deps.each { |raw_dep|
+        # it could be not Vertex, so do a lookup through data store
+        next unless Broker.data_store[raw_dep]
+
+        dep_data = Broker.data_store[raw_dep]
+        add_to_states_cache dep_data.vertex, dep_data.state
+        @graph.add_edge vertex, dep_data.vertex
+      }
+    end
+
+    private
+
+    # adds a given {Vertex} in state to @by_state cache;
+    # makes sure to remove it from the rest of the lists;
+    # lazily initializes the cache
+    def add_to_states_cache vertex, state
+      # TODO: shouldn't we add these vertices to the graph as "unresolved" up until they appear as {Vertex}
+      return unless state
+
+      @by_state.each_value { |v| v.delete vertex }
+      @by_state[state] ||= []
+      @by_state[state] << vertex
+    end
+  end
+end
---
+++ spec/vtracker_spec.rb
@@ -0,0 +1,60 @@
+# frozen_string_literal: true
+
+module ExcADG
+  describe VTracker do
+    subject(:dvmain) { double Vertex }
+    subject(:dvmain_data) { double VStateData::Full }
+    subject(:dvdep) { double Vertex }
+    subject(:dvdep_data) { double VStateData::Full }
+    subject(:dbroker) { class_double(Broker).as_stubbed_const(transfer_nested_constants: true) }
+    subject(:ddata_store) { double DataStore }
+    subject(:vtracker) { VTracker.new }
+
+    before {
+      allow(dvmain).to receive(:is_a?).with(Vertex).and_return true
+      allow(dvdep).to receive(:is_a?).with(Vertex).and_return true
+      allow(dvmain).to receive(:is_a?).with(Array).and_return false
+      allow(dvdep).to receive(:is_a?).with(Array).and_return false
+
+      allow(dbroker).to receive(:data_store).and_return ddata_store
+      %i[one two three four].each { |ghost_dep|
+        allow(ddata_store).to receive(:'[]').with(ghost_dep).and_return nil
+      }
+
+      allow(ddata_store).to receive(:'[]').with(dvmain).and_return dvmain_data
+      allow(ddata_store).to receive(:'[]').with(dvdep).and_return dvdep_data
+
+      allow(dvmain_data).to receive(:vertex).and_return dvmain
+      allow(dvdep_data).to receive(:vertex).and_return dvdep
+    }
+    it 'tracks vertices' do
+      allow(dvmain).to receive(:state).and_return :done
+      allow(dvdep).to receive(:state).and_return :failed
+      allow(dvmain_data).to receive(:state).and_return :done
+      allow(dvdep_data).to receive(:state).and_return :failed
+
+      vtracker.track dvmain, [:one, :two, dvdep]
+
+      expect(vtracker.by_state.keys).to eq %i[done failed]
+      expect(vtracker.by_state[:done]).to eq [dvmain]
+      expect(vtracker.by_state[:failed]).to eq [dvdep]
+      expect(vtracker.graph.vertices).to eq [dvmain, dvdep]
+
+      vtracker.track dvdep, %i[two three four]
+      expect(vtracker.by_state.keys).to eq %i[done failed]
+      expect(vtracker.by_state[:done]).to eq [dvmain]
+      expect(vtracker.by_state[:failed]).to eq [dvdep]
+
+      expect(vtracker.graph.vertices).to eq [dvmain, dvdep]
+    end
+
+    it 'ignores vertices without state' do
+      allow(dvmain).to receive(:state).and_return nil
+      allow(dvmain_data).to receive(:state).and_return nil
+
+      vtracker.track dvmain, %i[one two]
+
+      expect(vtracker.graph.vertices).to eq []
+    end
+  end
+end
